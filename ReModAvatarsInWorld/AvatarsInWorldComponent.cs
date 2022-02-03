8273536ac591882afdb20983e6a56e14b60deee2
Psychloor <blomqvistkevin@hotmail.com>
2022-02-03 11:04:24 +0100

    Test this way

--- ReModAvatarsInWorld/AvatarsInWorldComponent.cs
+++ ReModAvatarsInWorld/AvatarsInWorldComponent.cs
@@ -2,7 +2,16 @@
 {
 
     using System;
+    using System.Collections;
     using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+
+    using BestHTTP;
+
+    using Harmony;
+
+    using MelonLoader;
 
     using ReMod.Core;
     using ReMod.Core.Managers;
@@ -17,14 +26,17 @@
     
     // Thanks ReMod CE
     using AvatarList = Il2CppSystem.Collections.Generic.List<VRC.Core.ApiAvatar>;
+    using Harmony = HarmonyLib.Harmony;
 
     public sealed class AvatarsInWorldComponent : ModComponent, IAvatarListOwner
     {
-        private ReAvatarList worldAvatarList;
+        private ReAvatarList worldAvatarList; 
 
-        private readonly AvatarList foundAvatarsList = new();
+        private static readonly AvatarList FoundAvatarsList = new();
 
-        private readonly HashSet<string> foundAvatarIds = new();
+        private static readonly HashSet<string> FoundAvatarIds = new();
+
+        private readonly Harmony harmonyInstance;
 
         public override void OnUiManagerInit(UiManager uiManager)
         {
@@ -32,19 +44,51 @@
 
             // Since some worlds might have a fuck-ton of avatars, it's better to manually refresh.
             // Also since re-grabbing all avatars in world each time you open up the avatar menu isn't the best xD
-            Object.Destroy(worldAvatarList.GameObject.GetComponent<EnableDisableListener>());
+            // Object.Destroy(worldAvatarList.GameObject.GetComponent<EnableDisableListener>());
+        }
+
+        public AvatarsInWorldComponent()
+        {
+            harmonyInstance = new Harmony("AvatarsInWorldReMod");
+            
+            // Thanks Knah
+            foreach (var methodInfo in typeof(AvatarPedestal).GetMethods(BindingFlags.DeclaredOnly | BindingFlags.Instance | BindingFlags.Public).Where(it => it.Name.StartsWith("Method_Private_Void_ApiContainer_") && it.GetParameters().Length == 1))
+            {
+                harmonyInstance.Patch(
+                    methodInfo,
+                    GetType().GetMethod(
+                        nameof(AvatarPedestalPatch),
+                        BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.DeclaredOnly).ToNewHarmonyMethod());
+            }
+        }
+        
+        private static void AvatarPedestalPatch(ApiContainer __0)
+        {
+            if (__0.Code != 200) return;
+            var model = __0.Model?.TryCast<ApiAvatar>();
+            if (model == null) return;
+
+            if (model.releaseStatus.IndexOf("private", StringComparison.OrdinalIgnoreCase) != -1
+                && !model.authorId.Equals(APIUser.CurrentUser.id, StringComparison.Ordinal)) return;
+
+            if (!FoundAvatarIds.Contains(model.id))
+            {
+                FoundAvatarIds.Add(model.id);
+                FoundAvatarsList.Add(model);
+            }
         }
 
         public override void OnLeftRoom()
         {
-            foundAvatarsList.Clear();
-            foundAvatarIds.Clear();
+            FoundAvatarsList.Clear();
+            FoundAvatarIds.Clear();
         }
 
         public AvatarList GetAvatars(ReAvatarList avatarList)
         {
-            foreach (AvatarPedestal pedestal in Resources.FindObjectsOfTypeAll<AvatarPedestal>())
+            /*foreach (AvatarPedestal pedestal in Resources.FindObjectsOfTypeAll<AvatarPedestal>())
             {
+                // Patch into set of it and if not null add to founds?
                 var avatar = pedestal.field_Private_ApiAvatar_0;
                 if (avatar == null
                     || avatar.releaseStatus.IndexOf("private", StringComparison.OrdinalIgnoreCase) != -1
@@ -56,9 +100,9 @@
                     foundAvatarIds.Add(avatar.id);
                     foundAvatarsList.Add(pedestal.field_Private_ApiAvatar_0);
                 }
-            }
+            }*/
 
-            return foundAvatarsList;
+            return FoundAvatarsList;
         }
 
         public void Clear(ReAvatarList avatarList)
